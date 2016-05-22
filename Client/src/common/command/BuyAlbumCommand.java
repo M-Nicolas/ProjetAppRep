77f0807af88ccfb0a5e3ebd86e4f726507be0ad6
MEURGUES Nicolas <mn307776@etu.unice.fr>
2016-05-22 19:41:39 +0200

    Ajout d'une partie des commandes et des objets RMI nécéssaires au fonctionnement du projet

---
+++ Client/src/common/command/BuyAlbumCommand.java
@@ -0,0 +1,48 @@
+package common.command;
+
+import common.serviceRMI.MusicInterface;
+
+import java.rmi.RemoteException;
+import java.util.List;
+
+import javax.jms.JMSException;
+import javax.jms.Message;
+
+/**
+ * The BuyAlbum's command class
+ * @author Nicolas MEURGUES
+ *
+ */
+public class BuyAlbumCommand {
+
+    private String artist;
+    private String album;
+    private String adress;
+    private MusicInterface market;
+    private List<Message> messageList;
+
+    public BuyAlbumCommand(MusicInterface market, String artist, String album, String adress, List<Message> messageList) {
+        super();
+        this.artist = artist;
+        this.album = album;
+        this.adress = adress;
+        this.market = market;
+        this.messageList = messageList;
+    }
+
+    public void execute() throws RemoteException, JMSException {
+        System.out.println("→market.buyAlbum("+artist + ", " + album + ", " + adress + ")");
+        String albumDatas = market.buyAlbum(artist, album, adress);
+
+        if(album == null) {
+            System.out.println("An error occured while you were purchasing your album");
+        } else {
+            System.out.println("Your album will be delivered to this adress :\n" +
+                                adress+ "\n\nHere you have your tracking ID :\n\n" +
+                                albumDatas);
+        }
+
+
+
+    }
+}
